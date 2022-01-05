65bc97588c237bf6c727456eaaae119e5ccddf7c
KwekuDar <kwekudar@outlook.com>
2022-01-05 05:20:46 -0500

    final commit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,91 +4,98 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const stop = function(str) {
-    let div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
+//escaping a script
+const escape = function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
 };
 
-const createTweet = function(tweet) {
-    const time = moment(tweet.created_at).fromNow();
-    let $tweet = $(` 
-    <article>
-    <header>
-      <span class="user"><img class="user" src="${tweet.user.avatars}" alt="Face Logo Image">
-      <p>${tweet.user.name}</p></span>
-      <p class="handle">${tweet.user.handle}</p>
-    </header>
-      <p class="profile-tweet">${stop(tweet.content.text)}</p>
-    
-    <footer>
-      <span class ="time">${time}</span>
-      <span class ="buttons"> 
-        <i class="fas fa-flag"></i> 
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
-        </article>`);
+
+//create tweets
+const createTweetElement = function(tweetObj) {
+  const timeStamp = moment(tweetObj.created_at).fromNow();
+  let $tweet = $(` 
+  <article>
+  <header>
+    <span class="user"><img class="user" src="${tweetObj.user.avatars}" alt="Face Logo Image">
+    <p>${tweetObj.user.name}</p></span>
+    <p class="handle">${tweetObj.user.handle}</p>
+  </header>
+    <p class="profile-tweet">${escape(tweetObj.content.text)}</p>
   
-    return $tweet;
-  };
+  <footer>
+    <span class ="time">${timeStamp}</span>
+    <span class ="buttons"> 
+      <i class="fas fa-flag"></i> 
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+      </article>`);
 
-  const showTweets = function(tweets) {
-    for (let tweet of tweets) {
-      const newTweet = createTweet(tweet);
-      $("#tweets-container").prepend(newTweet);
+  return $tweet;
+};
+
+// load tweets with Ajax get request inside
+const loadTweets = function() {
+  $.ajax('/tweets', { method: 'GET' })
+      .then(function(tweets) {
+        // render function call for you
+        renderTweets(tweets);
+      });
+};
+  
+  loadTweets();
+  
+
+//render tweets
+const renderTweets = function(tweets) {
+  // loops through tweets
+  for (let tweet of tweets) {
+    let newTweet = createTweetElement(tweet);
+    $("#tweets-container").prepend(newTweet);
+  }
+};
+
+
+// doc ready
+$(document).ready(function() {
+
+  $("#submit").on("submit", function(event) {
+    // prevent the default behavior of the form submission
+    event.preventDefault();
+    
+    // Create a string in the format name=value&name=value...
+    const tweetData = $(this).serialize();
+    // Create a variable that finds the text length
+    const numbChar = $(".new-tweet").find("textarea").val().length;
+    //conditions if text is over 140 or empty
+    if (numbChar > 140) {
+      $(".error1").text("Character number exceeded").slideDown();
+    
+    } else if (numbChar === 0) {
+      $(".error2").text("Please write a message").slideDown();
+  
+    } else {
+      //slide up when error corrected
+      $(".error1").slideUp();
+      $(".error2").slideUp();
+      //ajax POST
+      $.post("/tweets", tweetData, function(data) {
+      })
+      // rest everything for when data fetch
+        .done(function() {
+          $("#submit").trigger("reset");
+          $(".counter").html(140);
+          // function called for get request
+          $.ajax('/tweets', { method: 'GET' })
+      .then(function(tweets) {
+        // render function call for you
+        window.location.reload();
+      });
+
+        });
     }
-  };
-
-  const loadTweets = function() {
-    $.ajax({
-      url: "/tweets",
-      method: "GET"
-    })
-      .done((data) => {
-        showTweets(data);
-      })
-      .fail((err) => {
-        console.log(err.message);
-      })
-      .always(() => console.log('request to API has been fulfilled'));
-  };
-
-  $(document).ready(function() {
-
-    $("form").on("submit", function(event) {
-      // prevent the default behavior of the form submission
-      event.preventDefault();
-      console.log("Submit form");
-      
-      
-      const tweetData = $(this).serialize();
-      
-      const numbChar = $(".new-tweet").find("textarea").val().length;
-      
-      if (numbChar > 140) {
-        $(".error1").text("Character number exceeded").slideDown();
-      
-      } else if (numbChar === 0) {
-        $(".error2").text("Please write a message").slideDown();
-    
-      } else {
-        
-        $(".error1").slideUp();
-        $(".error2").slideUp();
-        //ajax POST
-        $.ajax({
-          type: "POST",
-          url: "/tweets",
-          data: tweetData,
-        }).then(loadTweets)
-          .catch(res => console.log(res));
-        //clear form
-        $(".new-tweet").find("form").trigger("reset");
-        //reset counter
-        $(".counter").text(140);
-      }
-    });
-    
-    loadTweets();
-    
   });
+
+
+});
